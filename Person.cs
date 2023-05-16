81b6ec9de63ee8ea18fdf10827950ad90c36e15f
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-05-16 08:27:53 -0600

    Added Person class

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer_Data
+{
+    internal class Person
+    {
+        public Person(string name, string address, string phone)
+        {
+            Name = name;
+            Address = address;
+            Phone = phone;
+        }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
